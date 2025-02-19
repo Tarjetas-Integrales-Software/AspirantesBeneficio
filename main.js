fb8954e8dc0b06aea37106994d1a270dcace57da
David Madrid Napoles <madridnapolesdavid17100201@gmail.com>
2025-02-19 15:29:09 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Cargar la aplicación Angular
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, 'dist/aspirantes-beneficio/browser/index.html'),
+      protocol: 'file:',
+      slashes: true,
+    })
+  );
+
+  // Abrir las herramientas de desarrollo (opcional)
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
