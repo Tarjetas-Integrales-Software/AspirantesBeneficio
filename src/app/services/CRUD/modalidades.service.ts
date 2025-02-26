379aed2b4942e5c3e32da54eb9d6d2745b8c3425
David Madrid Napoles <madridnapolesdavid17100201@gmail.com>
2025-02-26 16:32:46 -0600

    Se agregan metodos para modalidades

---
+++ src/app/services/CRUD/modalidades.service.ts
@@ -0,0 +1,53 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DatabaseService } from '../../services/database.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalidadesService {
+  private http = inject(HttpClient);
+
+  constructor(private databaseService: DatabaseService) {
+
+  }
+
+  getModalidades(): Observable<any> {
+    return this.http.get(environment.apiUrl + '/lic/modalidades_with_joins');
+  }
+
+  async syncLocalDataBase(datos: any[]): Promise<void> {
+    for (const item of datos) {
+      const sql = `
+        INSERT INTO cat_ct_modalidades (
+          id_tipo_beneficio, nombre, descripcion, imagen,
+          created_id, updated_id, deleted_id, created_at, updated_at, deleted_at
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+      `;
+      const params = [
+        item.id_tipo_beneficio,
+        item.nombre,
+        item.descripcion,
+        item.imagen,
+        item.created_id,
+        item.updated_id,
+        item.deleted_id,
+        item.created_at,
+        item.updated_at,
+        item.deleted_at,
+      ];
+
+      await this.databaseService.execute(sql, params);
+    }
+  }
+
+  async consultarModalidades(): Promise<{ id: number; modalidad: string }[]> {
+    const sql = 'SELECT id, modalidad FROM cat_ct_modalidades ORDER BY modalidad;';
+
+    // Ejecutar la consulta
+    const resultados = await this.databaseService.query(sql);
+    return resultados;
+  }
+}
