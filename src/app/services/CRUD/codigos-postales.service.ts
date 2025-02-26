cc44baba56db50b328effdce261fc1bae7d8895c
dmadridtisa1 <madridnapolesdavid17100201@gmail.com>
2025-02-26 09:50:53 -0600

    Config (#9)
    * Mejorar la funcionalidad de captura de fotos y actualizar el diseño de la interfaz de usuario (#3)
    
    * Ajustes a version y autoupdate
    
    * Se quita la consola y las opciones del menu en la aplicacion de electron
    
    * Avance BD
    
    * Commit necesario
    
    * Avance
    
    * Se agrega el cerrar sesion
    
    * c
    
    ---------
    
    Co-authored-by: Néstor Daniel Davalos <danessseguro@gmail.com>

--- src/app/services/CRUD/codigos-postales.service.ts
+++ src/app/services/CRUD/codigos-postales.service.ts
@@ -2,7 +2,6 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { StorageService } from '../../services/storage.service';
 import { DatabaseService } from '../../services/database.service';
 
 @Injectable({
