393c12466cf0bcd0eea5df5e6e6d7e9d0f17ee73
Ericardo Munoz Diaz <emunoz@tarjetasintegrales.mx>
2025-03-04 17:55:15 -0600

    se agrega tabla y service en crud para las curps registradas ya en tisa, se envien hacia las DBs Locales en cada aplicacion

---
+++ src/app/services/CRUD/curps-registradas.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DatabaseService } from '../../services/database.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurpsRegistradasService {
+
+  private http = inject(HttpClient);
+
+  constructor(private databaseService: DatabaseService) { }
+
+  getCurpsRegistradas(): Observable<any> {
+    return this.http.get(environment.apiUrl + '/lic/aspben/curps_registradas');
+  }
+
+  async syncLocalDataBase(datos: any[]): Promise<void> {
+    for (const item of datos) {
+      const sql = `
+        INSERT OR IGNORE INTO cat_curps_registradas (
+        curp
+        ) VALUES (?)
+      `;
+      const params = [
+        item.curp,
+      ];
+
+      await this.databaseService.execute(sql, params);
+    }
+  }
+}
