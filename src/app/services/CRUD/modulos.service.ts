8e33d1a90381df17901ce2b5f7f382e54c4a73f2
dmadridtisa1 <madridnapolesdavid17100201@gmail.com>
2025-03-12 16:25:22 -0600

    Config (#25)
    * Se ven solo las acciones cuando se tiene permiso
    
    * Nueva version
    
    * Se mejora grid de consulta

---
+++ src/app/services/CRUD/modulos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModulosService {
+  private http = inject(HttpClient);
+
+  constructor() {
+
+  }
+
+  getModulos(): Observable<any> {
+    return this.http.get(environment.apiUrl + '/lic/modulos_with_joins');
+  }
+}
