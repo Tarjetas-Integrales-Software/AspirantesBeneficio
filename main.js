c665d5fa191045ca7c0d81d1773d96e68b677743
David Madrid Napoles <madridnapolesdavid17100201@gmail.com>
2025-03-13 14:12:59 -0600

    Ajusta version

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function createWindow() {
   });
 
   // Abre consola
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Cargar la aplicación Angular
   mainWindow.loadURL(
