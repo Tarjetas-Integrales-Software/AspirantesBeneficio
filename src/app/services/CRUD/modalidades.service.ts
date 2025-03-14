ecef4120c30c3ea132f30813758c5ae6ebfea8cc
DanNess-system <danessseguro@gmail.com>
2025-03-14 12:22:20 -0600

    Refactor configuraciones and datos-generales components to improve navigation and data synchronization; enhance modalidades service for offline support

--- src/app/services/CRUD/modalidades.service.ts
+++ src/app/services/CRUD/modalidades.service.ts
@@ -3,19 +3,45 @@ import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { DatabaseService } from '../../services/database.service';
+import { NetworkStatusService } from '../network-status.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalidadesService {
   private http = inject(HttpClient);
+  private networkStatusService = inject(NetworkStatusService);
 
   constructor(private databaseService: DatabaseService) {
 
   }
 
   getModalidades(): Observable<any> {
-    return this.http.get(environment.apiUrl + '/lic/modalidades_with_joins');
+    return new Observable(observer => {
+      this.networkStatusService.isOnline.subscribe(online => {
+        if (online) {
+          this.http.get(environment.apiUrl + '/lic/modalidades_with_joins').subscribe({
+            next: (response) => {
+              observer.next(response);
+              observer.complete();
+            },
+            error: (error) => {
+              observer.error(error);
+            }
+          });
+        } else {
+          const sql = 'SELECT * FROM cat_ct_modalidades WHERE id_tipo_beneficio = 2';
+          const params: any[] = [];
+
+          this.databaseService.query(sql, params).then(resultados => {
+            observer.next({ data: resultados });
+            observer.complete();
+          }).catch(error => {
+            observer.error(error);
+          });
+        }
+      });
+    });
   }
 
   async syncLocalDataBase(datos: any[]): Promise<void> {
