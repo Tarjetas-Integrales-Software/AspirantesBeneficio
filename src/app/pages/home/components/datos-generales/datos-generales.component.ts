9b79665f0df518deaec0e9ad5042ff5ff4a98378
Néstor Daniel Davalos <danessseguro@gmail.com>
2025-03-14 13:59:44 -0600

    Refactor postal code filtering logic to simplify code and improve readability (#37)
    * Refactor synchronization logic and enhance footer component; update sync status handling
    
    * Refactor sync status handling; use environment variable for sync interval and implement computed signal for image status
    
    * Enhance configuraciones component: add router navigation after successful update and improve UI styling for modulo display
    
    * Ajustar diseño de componentes y mejorar la estructura del footer; optimizar espaciado en consulta y menú
    
    * Refactor configuraciones and datos-generales components to improve navigation and data synchronization; enhance modalidades service for offline support
    
    * Add offline support for postal code retrieval and improve module selection feedback
    
    * Refactor postal code retrieval to streamline function calls and enhance async handling
    
    * Mejora la función de obtención de códigos postales para manejar correctamente los municipios y optimiza el filtrado de resultados.
    
    * Refactor postal code filtering logic to simplify code and improve readability

--- src/app/pages/home/components/datos-generales/datos-generales.component.ts
+++ src/app/pages/home/components/datos-generales/datos-generales.component.ts
@@ -221,55 +221,49 @@ export class DatosGeneralesComponent implements OnInit {
 
   municipios: any[] = [];
   municipio: string = '';
+  online: boolean = false;
 
   private router = inject(Router);
   private activatedRoute = inject(ActivatedRoute);
 
   ngOnInit(): void {
     this.modulo_actual = this.configuracionesService.getSelectedValueModu();
-    if(!this.modulo_actual) {
+    if (!this.modulo_actual) {
       let timerInterval: NodeJS.Timeout;
-          Swal.fire({
-            icon: 'info',
-            title: 'Aún no seleccionás un módulo...',
-            html: 'Por favor, seleccioné un módulo y vuelva a intentar <br> Redirigiendo en <b></b> segundos.',
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            timer: 5000,
-            timerProgressBar: true,
-            didOpen: () => {
-              Swal.showLoading();
-              const container = Swal.getHtmlContainer();
-              if (!container) return; // Verifica que no sea null
-              const b = container.querySelector('b') as HTMLElement | null;
-              if (!b) return; // Verifica que 'b' exista
+      Swal.fire({
+        icon: 'info',
+        title: 'Aún no seleccionás un módulo...',
+        html: 'Por favor, seleccioné un módulo y vuelva a intentar <br> Redirigiendo en <b></b> segundos.',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        timer: 5000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const container = Swal.getHtmlContainer();
+          if (!container) return; // Verifica que no sea null
+          const b = container.querySelector('b') as HTMLElement | null;
+          if (!b) return; // Verifica que 'b' exista
 
-              timerInterval = setInterval(() => {
-                const timerLeft = Swal.getTimerLeft(); // Puede ser undefined
-                if (typeof timerLeft === 'number') { // Verifica si es un número
-                  b.textContent = Math.ceil(timerLeft / 1000).toString();
-                }
-              }, 1000);
-            },
-            willClose: () => {
-              clearInterval(timerInterval);
+          timerInterval = setInterval(() => {
+            const timerLeft = Swal.getTimerLeft(); // Puede ser undefined
+            if (typeof timerLeft === 'number') { // Verifica si es un número
+              b.textContent = Math.ceil(timerLeft / 1000).toString();
             }
-          }).then((result) => {
-            if (result.dismiss === Swal.DismissReason.timer) {
-              this.router.navigateByUrl('/inicio/configuraciones');
-            }
-          });
-    }
-    const online = this.networkStatusService.checkConnection();
-    if (online) {
-      this.syncDataBase();
-    } else {
-      this.codigosPostalesService.consultarCodigosPostales({}).then(codigos => {
-        this.allCodigosPostales = codigos;
-      }).catch(error => {
-        console.error('Error al consultar códigos postales:', error);
+          }, 1000);
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        }
+      }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+          this.router.navigateByUrl('/inicio/configuraciones');
+        }
       });
     }
+    this.online = this.networkStatusService.checkConnection();
+    console.log(this.online, 'estatus online')
+    if (this.online) this.syncDataBase();
     this.getMunicipios();
     this.getModalidades();
     this.getGrados();
@@ -404,19 +398,20 @@ export class DatosGeneralesComponent implements OnInit {
       .catch((error) => console.error('Error al obtener municipios:', error));
   }
 
-  getCodigosPostales(params: { municipio?: string }): void {
+  async getCodigosPostales(params: { municipio?: string }): Promise<void> {
     const { municipio } = params;
 
-    if (municipio) {
-      const filtered = this.allCodigosPostales.filter(cp => cp.municipio.includes(municipio));
-      this.codigosPostales = Array.from(new Set(filtered.map(cp => cp.cp))).map(cp => {
-        return filtered.find(item => item.cp === cp);
-      });
-    } else {
-      this.codigosPostales = Array.from(new Set(this.allCodigosPostales.map(cp => cp.cp))).map(cp => {
-        return this.allCodigosPostales.find(item => item.cp === cp);
-      });
-    }
+    await this.codigosPostalesService.consultarCodigosPostales({ municipio }).then(codigos => {
+      console.log('Codigos postales:', codigos, 'Municipio:', municipio);
+      this.allCodigosPostales = codigos;
+      console.log('All codigos postales:', this.allCodigosPostales);
+    }).catch(error => {
+      console.error('Error al consultar códigos postales:', error);
+    });
+
+    this.codigosPostales = Array.from(new Set(this.allCodigosPostales.map(cp => cp.cp))).map(cp => {
+      return this.allCodigosPostales.find(item => item.cp === cp);
+    });
   }
 
   getColoniasByCP(): void {
