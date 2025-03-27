0090af4c5f96a57b383718251f0781c3185c46a5
Ericardo Munoz Diaz <emunoz@tarjetasintegrales.mx>
2025-03-27 15:17:58 -0600

    se quita mi usuario de login y los devtools

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function createWindow() {
   });
 
   // Abre consola
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Cargar la aplicación Angular
   mainWindow.loadURL(
