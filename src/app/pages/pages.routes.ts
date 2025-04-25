e8dbb148caadd95e537f784e2ddbd63a15b8ed55
DanNess-system <danessseguro@gmail.com>
2025-04-25 11:09:41 -0600

    feat: add ImpresionManual component with basic template and styles

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -7,6 +7,7 @@ import { ConfiguracionesComponent } from "./configuraciones/configuraciones.comp
 import { AsistenciaComponent } from "./asistencia/asistencia.component";
 import { ImpresionCredencialComponent } from "./impresion-credencial/impresion-credencial.component";
 import { AccionesComponent } from "./acciones/acciones.component";
+import { ImpresionManualComponent } from "./impresionManual/impresionManual.component";
 
 export const pagesRoutes: Routes = [
     {
@@ -47,11 +48,16 @@ export const pagesRoutes: Routes = [
               path: 'acciones',
               title: 'Acciones',
               component: AccionesComponent
-          },
+            },
             {
                 path: 'impresion-credencial',
                 title: 'Impresion Credencial',
                 component: ImpresionCredencialComponent
+            },
+            {
+                path: 'impresion-manuales',
+                title: 'Impresion Credencial Manuales',
+                component: ImpresionManualComponent
             },
             {
                 path: '**',
