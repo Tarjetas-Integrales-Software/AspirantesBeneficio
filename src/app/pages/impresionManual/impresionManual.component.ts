6383d531147736b047ee28869e6985a0768c2b6e
Néstor Daniel Davalos <danessseguro@gmail.com>
2025-04-25 22:27:55 -0600

    feat: add ImpresionManual component with basic template and styles (#53)
    * Refactor synchronization logic and enhance footer component; update sync status handling
    
    * Refactor sync status handling; use environment variable for sync interval and implement computed signal for image status
    
    * Enhance configuraciones component: add router navigation after successful update and improve UI styling for modulo display
    
    * Ajustar diseño de componentes y mejorar la estructura del footer; optimizar espaciado en consulta y menú
    
    * Refactor configuraciones and datos-generales components to improve navigation and data synchronization; enhance modalidades service for offline support
    
    * Add offline support for postal code retrieval and improve module selection feedback
    
    * Refactor postal code retrieval to streamline function calls and enhance async handling
    
    * Mejora la función de obtención de códigos postales para manejar correctamente los municipios y optimiza el filtrado de resultados.
    
    * Refactor postal code filtering logic to simplify code and improve readability
    
    * Add photo ID to Aspirante model, update navigation and enhance photo handling in Foto component
    
    * Add logging for aspirante photo retrieval, remove unused Jalisco interface, and include event date in form data
    
    * Refactor photo handling in Foto component: streamline form editing process and enhance logging in aspirante service
    
    * Implementar creación y actualización de fotos en el componente Foto: registrar nueva foto en la base de datos y actualizar relación con el aspirante
    
    * Agregar funcionalidad para subir fotos al servidor en el componente Foto: registrar nueva foto y mejorar manejo de errores
    
    * Corregir acceso al ID de la foto en la respuesta del servicio y agregar funcionalidad para guardar la foto en el directorio local antes de subirla al servidor
    
    * editar registro y foto completo
    
    * Actualizar el componente de inicio de sesión con credenciales predeterminadas y agregar funcionalidad para la carga de archivos PDF en el componente Foto
    
    * Agregar funcionalidad para guardar archivos PDF y crear tabla ct_fotos en la base de datos
    
    * Creacion de tabla ct_documentos y relacion entre aspirantes
    
    * configuracion en el package para iniciar la aplicacion en desarollo
    
    * Inicio de sincronizacion de documentos cada minuto (falta guardar bien el archivo pdf)
    
    * subida de archivo en la carpeta local y creacion de la relacion entre documento y foto en bd local junto con sus servicios de creacion y relacion
    
    * Mejoras en la gestión de documentos: correcciones en la lectura y registro de archivos PDF, optimización del manejo de errores y limpieza de código. (ya sube el pdf al backend correctamente)
    
    * Actualiza formularios para eliminar datos predeterminados y mejora la lógica de carga de documentos.
    
    * Elimina la sincronización de documentos cada minuto y ajusta la lógica de sincronización de CURPs para incluir la sincronización de documentos.
    
    * Actualiza la asignación del código postal en Datos Generales para usar el valor del aspirante en lugar de la selección.
    
    * Agrega lógica para extraer y establecer la fecha de nacimiento a partir del CURP ingresado y habilita la opción de subir documentos condicionalmente en el componente de foto.
    
    * Elimina validaciones mínimas para tipo_zona y tipo_asentamiento en el formulario de Datos Generales.
    
    * Actualiza la configuración de Electron y mejora la gestión de opciones de menú en el componente de inicio de sesión, se añade icono para mostrar u ocultar contraseña
    
    * feat: add ImpresionManual component with basic template and styles
    
    * feat: mejora en la gestión de impresión manual y ajustes en la configuración del proyecto
    
    * feat: mejora la gestión de datos en el componente Foto y simplifica la plantilla de ImpresionManual
    
    * feat: mejora en la interfaz de usuario y gestión de imágenes en el componente de impresión manual

--- src/app/pages/impresionManual/impresionManual.component.ts
+++ src/app/pages/impresionManual/impresionManual.component.ts
@@ -1,14 +1,142 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, type OnInit, ViewChild, type ElementRef, Input, Output, EventEmitter, signal } from "@angular/core"
+import { CommonModule } from "@angular/common"
+import { FormGroup, FormsModule } from "@angular/forms"
+import { HttpClient } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import Swal from 'sweetalert2';
 
+const { ipcRenderer } = (window as any).require("electron");
 @Component({
   selector: 'app-impresion-manual',
-  imports: [],
+  imports: [CommonModule, FormsModule, MatInputModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './impresionManual.component.html',
   styles: `
     :host {
       display: block;
     }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImpresionManualComponent { }
+export class ImpresionManualComponent implements OnInit {
+  @ViewChild("videoElement") videoElement!: ElementRef<HTMLVideoElement>
+  @ViewChild("canvas") canvas!: ElementRef<HTMLCanvasElement>
+
+  @Input() buttonTitle: string = "Capturar";
+  @Input() disabledRegister: boolean = true;
+
+  @Output() submitForm = new EventEmitter<void>();
+
+  @Output() buttonClicked = new EventEmitter<void>();
+
+  devices: MediaDeviceInfo[] = []
+  selectedDevice = ""
+  capturedImage = signal<string | null>(null)
+  stream: MediaStream | null = null
+  imageFormat: "jpeg" | "webp" = "webp"
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getAvailableCameras()
+  }
+
+  notifyParent() {
+    this.buttonClicked.emit();
+  }
+
+  async getAvailableCameras() {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices()
+      this.devices = devices.filter((device) => device.kind === "videoinput")
+    } catch (error) {
+      console.error("Error accessing media devices:", error)
+    }
+  }
+
+  async startStream() {
+    if (this.stream) {
+      this.stopStream()
+    }
+
+    try {
+      this.stream = await navigator.mediaDevices.getUserMedia({
+        video: { deviceId: this.selectedDevice },
+      })
+      this.videoElement.nativeElement.srcObject = this.stream
+    } catch (error) {
+      console.error("Error accessing the camera:", error)
+    }
+  }
+
+  stopStream() {
+    if (this.stream) {
+      this.stream.getTracks().forEach((track) => track.stop())
+      this.stream = null
+    }
+    this.videoElement.nativeElement.srcObject = null
+  }
+
+  async capturePhoto() {
+    const video = this.videoElement.nativeElement
+    const canvas = this.canvas.nativeElement
+    canvas.width = video.videoWidth
+    canvas.height = video.videoHeight
+    canvas.getContext("2d")?.drawImage(video, 0, 0, canvas.width, canvas.height)
+    // Convert to WebP if supported, otherwise fallback to JPEG
+    if (this.imageFormat === "webp") {
+      this.capturedImage.set(canvas.toDataURL("image/webp"))
+      if (this.capturedImage() === "data:,") {
+        // WebP is not supported, fallback to JPEG
+        this.imageFormat = "jpeg"
+        this.capturedImage.set(canvas.toDataURL("image/jpeg"))
+      }
+    } else {
+      this.capturedImage.set(canvas.toDataURL("image/jpeg"))
+    }
+
+  }
+
+  savePhoto(name: string, path: string) {
+    if (this.capturedImage()) {
+      ipcRenderer.send("save-image", this.capturedImage(), name, path);
+    }
+  }
+
+  toggleImageFormat() {
+    this.imageFormat = this.imageFormat === "webp" ? "jpeg" : "webp"
+    if (this.capturedImage()) {
+      this.capturePhoto() // Re-capture with new format-
+    }
+  }
+
+  mostrarErrores(form: FormGroup) {
+    let errorMessages = '';
+    Object.keys(form.controls).forEach(key => {
+      const control = form.get(key);
+      const controlErrors = control ? control.errors : null;
+      if (controlErrors != null) {
+        Object.keys(controlErrors).forEach(keyError => {
+          const errorMessage = `Error en el control ${key}: ${keyError}, valor: ${controlErrors[keyError]}`;
+          console.log(errorMessage);
+          errorMessages += `${errorMessage}\n`;
+        });
+      }
+    });
+
+    if (errorMessages) {
+      Swal.fire({
+        title: 'Errores en el formulario',
+        text: errorMessages,
+        icon: 'error',
+        confirmButtonText: 'Aceptar'
+      });
+    }
+  }
+
+  async onSubmit(): Promise<void> { }
+
+  deleteImage() {
+    this.capturedImage.set(null);
+  }
+}
