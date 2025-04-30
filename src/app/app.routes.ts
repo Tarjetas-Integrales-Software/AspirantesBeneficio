f8d63189bc4f5ca349b28c511fccb9bf87bb8301
Ericardo Munoz Diaz <emunoz@tarjetasintegrales.mx>
2025-04-29 18:03:34 -0600

    se sube el cambio en el routes.ts para ingresar directamente a login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,5 @@ export const routes: Routes = [
     path: 'inicio',
     loadChildren: () => import('./pages/pages.routes').then(m => m.pagesRoutes)
   },
-  { path: '**', redirectTo: '/inicio/digitalizador', pathMatch: 'full' }
+  { path: '**', redirectTo: '/auth/login', pathMatch: 'full' }
 ];
